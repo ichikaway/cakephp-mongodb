ccbd21568b8c5f77bac22c360c0e885112db04c3
Yasushi Ichikawa <ichikaway@gmail.com>
2010-01-16 20:41:44 +0900

    fix indent

--- models/datasources/mongodb_source.php
+++ models/datasources/mongodb_source.php
@@ -28,49 +28,49 @@ class MongodbSource extends DataSource{
 
 	protected $_db = null;
 
-    public function __construct($config = array()) {
-        $defaults = array(
-            'persistent' => true,
-            'host'       => 'localhost',
-            'database'   => '',
-            'port'       => '27017',
-        );
-        parent::__construct(array_merge( $defaults, $config));
+	public function __construct($config = array()) {
+		$defaults = array(
+				'persistent' => true,
+				'host'       => 'localhost',
+				'database'   => '',
+				'port'       => '27017',
+				);
+		parent::__construct(array_merge( $defaults, $config));
 
 		return $this->connect();
 	}
 
-    public function connect() {
-        $config = $this->config;
-        $this->connected = false;
-        $host = $config['host'] . ':' . $config['port'];
-        $this->connection = new Mongo($host, true, $config['persistent']);
-        if ($this->_db = $this->connection->selectDB($config['database'])) {
-            $this->connected = true;
-        }
-        return $this->connected;
-    }
+	public function connect() {
+		$config = $this->config;
+		$this->connected = false;
+		$host = $config['host'] . ':' . $config['port'];
+		$this->connection = new Mongo($host, true, $config['persistent']);
+		if ($this->_db = $this->connection->selectDB($config['database'])) {
+			$this->connected = true;
+		}
+		return $this->connected;
+	}
 
 	public function close(){
 		return $this->disconnect();
 	}	
 
-    public function disconnect() {
-        if ($this->connected) {
-            $this->connected = !$this->connection->close();
-            unset($this->_db);
-            unset($this->connection);
-            return !$this->connected;
-        }
-        return true;
-    }
+	public function disconnect() {
+		if ($this->connected) {
+			$this->connected = !$this->connection->close();
+			unset($this->_db);
+			unset($this->connection);
+			return !$this->connected;
+		}
+		return true;
+	}
 
-    public function listSource($data = null) {
-        return $this->_db->listCollections();
-    }
+	public function listSource($data = null) {
+		return $this->_db->listCollections();
+	}
 
-	
-    public function read(&$model, $query = array()) {
+
+	public function read(&$model, $query = array()) {
 		$query = $this->_setEmptyArrayIfEmpty($query);
 		extract($query);
 
@@ -79,7 +79,7 @@ class MongodbSource extends DataSource{
 		}
 
 		$result = $this->_db->selectCollection($model->table)->find($conditions, $fields)
-				  ->sort($order)->limit($limit)->skip( ($page - 1)  * $limit);
+					->sort($order)->limit($limit)->skip( ($page - 1)  * $limit);
 
 		if($model->findQueryType === 'count'){
 			return array( array($model->name => array('count' =>  $result->count())) );
@@ -95,7 +95,7 @@ class MongodbSource extends DataSource{
 		}
 		return $results;
 
-    }
+	}
 
 	protected function _setEmptyArrayIfEmpty($data){
 		if(is_array($data)){
