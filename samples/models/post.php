4effd3374843514d5cf14a6045aea697ab40c67e
Yasushi Ichikawa <ichikaway@gmail.com>
2010-01-17 01:02:14 +0900

    change field type

--- samples/models/post.php
+++ samples/models/post.php
@@ -4,7 +4,11 @@ class Post extends AppModel {
 
 	var $useTable = false;
 
-	var $_schema = array('title' => array('type'=>'text'),'body'=>array('type'=>'text'),'hoge'=>array('type'=>'text'));	
+	var $_schema = array(
+			'title' => array('type'=>'string'),
+			'body'=>array('type'=>'string'),
+			'hoge'=>array('type'=>'string')
+			);	
 
 }
 
