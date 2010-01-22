0f04eca04f506f71d8a400f55b852f12e3671b1c
Yasushi Ichikawa <ichikaway@gmail.com>
2010-01-22 15:38:38 +0900

    change sample

--- samples/models/post.php
+++ samples/models/post.php
@@ -8,6 +8,8 @@ class Post extends AppModel {
 			'title' => array('type'=>'string'),
 			'body'=>array('type'=>'string'),
 			'hoge'=>array('type'=>'string'),
+			'created'=>array('type'=>'date'),
+			'modified'=>array('type'=>'date'),
 			);	
 
 }
