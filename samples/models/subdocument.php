147a1d08dd54097f0fcfbbf7b5581da733530efb
Yasushi Ichikawa <ichikaway@gmail.com>
2010-05-20 13:16:00 +0900

    change sample code

--- samples/models/subdocument.php
+++ samples/models/subdocument.php
@@ -4,10 +4,13 @@ class Subdocument extends AppModel {
 	var $mongoSchema = array(
 			'title' => array('type'=>'string'),
 			'body'=>array('type'=>'string'),
-			'subdoc'=>array('type'=>'string'),
+			'subdoc'=>array(
+				'name' => array('type'=>'string'),
+				'age' => array('type'=>'integer')
+			),
 			'created'=>array('type'=>'date'),
 			'modified'=>array('type'=>'date'),
-			);	
+			);
 
 }
 
