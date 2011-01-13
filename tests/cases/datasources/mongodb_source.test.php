8c9a54cec97994239c0eb6acfa08578a55e29fd0
AD7six <andydawson76@yahoo.co.uk>
2011-01-13 17:36:57 +0100

    removing unnecessary method override

--- tests/cases/datasources/mongodb_source.test.php
+++ tests/cases/datasources/mongodb_source.test.php
@@ -76,41 +76,6 @@ class Post extends AppModel {
 		}
  		return false;
  	}
-
-	function isUnique($fields, $or = true) {
-		if (!is_array($fields)) {
-			$fields = func_get_args();
-			if (is_bool($fields[count($fields) - 1])) {
-				$or = $fields[count($fields) - 1];
-				unset($fields[count($fields) - 1]);
-			}
-		}
-
-		foreach ($fields as $field => $value) {
-			if (is_numeric($field)) {
-				unset($fields[$field]);
-
-				$field = $value;
-				if (isset($this->data[$this->alias][$field])) {
-					$value = $this->data[$this->alias][$field];
-				} else {
-					$value = null;
-				}
-			}
-
-			if (strpos($field, '.') === false) {
-				unset($fields[$field]);
-				$fields[$this->alias . '.' . $field] = $value;
-			}
-		}
-		if ($or) {
-			$fields = array('$or' => $fields);
-		}
-		if (!empty($this->id)) {
-			$fields[$this->alias . '.' . $this->primaryKey . ' !='] =  $this->id;
-		}
-		return ($this->find('count', array('conditions' => $fields, 'recursive' => -1)) == 0);
-	}
 }
 
 /**
