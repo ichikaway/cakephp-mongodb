7f3598a93e590e53a807c7ddaf0e7fb8a8a1f8ca
ciccimanolesta <ciccimanolesta@yahoo.it>
2012-09-05 15:30:33 +0300

    Update Model/Datasource/MongodbSource.php
    In Mondo PHP Driver >= 1.2.0 connections are persistent. The parameter was removed.

--- Model/Datasource/MongodbSource.php
+++ Model/Datasource/MongodbSource.php
@@ -182,7 +182,7 @@ class MongodbSource extends DboSource {
 			if (isset($this->config['replicaset']) && count($this->config['replicaset']) === 2) {
 				$this->connection = new Mongo($this->config['replicaset']['host'], $this->config['replicaset']['options']);
 			} else if ($this->_driverVersion >= '1.2.0') {
-				$this->connection = new Mongo($host, array("persist" => $this->config['persistent']));
+				$this->connection = new Mongo($host);
 			} else {
 				$this->connection = new Mongo($host, true, $this->config['persistent']);
 			}
